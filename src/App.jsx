2e6b9036dc2478a0b73ef31ffb9eac2eb9cb0622
Himefe <idanger2015@hotmail.com>
2022-07-24 23:18:13 -0300

    Criação dos itens de chat, chatIntro...

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
 import React from "react";
 import styles from "./App.module.css";
 
-import { DonutLarge, Chat, MoreHoriz, Search } from "@mui/icons-material";
+import {
+  DonutLarge,
+  Chat,
+  MoreHoriz,
+  Search,
+  FilterList,
+} from "@mui/icons-material";
+import ChatItem from "./Components/ChatItem/ChatItem";
+import ChatIntro from "./Components/ChatIntro/ChatIntro";
 
 const App = () => {
+  const chatList = [
+    {
+      chatId: 1,
+      nome: "Irineu Jubileu",
+      avatar: "https://cdn-icons-png.flaticon.com/512/1253/1253756.png",
+      mensagens: ["HAHAHA", "EAI MEU AMIGO", "KKAKAKAKA", "Boa noite!!!"],
+    },
+    {
+      chatId: 2,
+      nome: "Alírio Sampaio",
+      avatar: "https://cdn-icons-png.flaticon.com/512/147/147140.png",
+      mensagens: [
+        "HAHAHA",
+        "EAI MEU AMIGO",
+        "KKAKAKAKA",
+        "Moro na rua 2, do lado da rua 1",
+      ],
+    },
+    {
+      chatId: 3,
+      nome: "Jorge Zebra",
+      avatar: "https://cdn-icons-png.flaticon.com/512/147/147142.png",
+      mensagens: ["HAHAHA", "EAI MEU AMIGO", "KKAKAKAKA", "HAHAHAHAHAHA"],
+    },
+  ];
+
+  const [activeChatId, setActiveChatId] = React.useState();
+
   return (
     <div className={styles.app_window}>
       <aside className={styles.sidebar}>
@@ -30,15 +66,28 @@ const App = () => {
           <div className={styles.search_input_area}>
             <Search />
             <input
-              type="text"
+              type="search"
               name="search_header"
               id="search_header"
               placeholder="Pesquisar ou começar uma nova conversa"
             />
           </div>
+          <FilterList />
+        </div>
+        <div className={`${styles.chatlist_area}`}>
+          {chatList.map((item) => (
+            <ChatItem
+              key={item.chatId}
+              setActiveChatId={setActiveChatId}
+              chat={item}
+              active={activeChatId === item.chatId}
+            />
+          ))}
         </div>
       </aside>
-      <main className={styles.content_area}>A</main>
+      <main className={styles.content_area}>
+        <ChatIntro />
+      </main>
     </div>
   );
 };
---
+++ src/Components/ChatItem/ChatItem.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import AppStyles from "../../App.module.css";
+import styles from "./ChatItem.module.css";
+
+const ChatItem = ({ active, setActiveChatId, chat }) => {
+  const handleClickRef = (e) => {
+    setActiveChatId(chat.chatId);
+  };
+
+  return (
+    <div
+      className={`${styles.chatItem}`}
+      onClick={handleClickRef}
+      data-active={active}
+    >
+      <div className={styles.avatar}>
+        <img src={chat.avatar} alt="Avatar ícone" />
+      </div>
+      <div className={styles.detalhes_mensagens}>
+        <div className={styles.nome_e_mensagem}>
+          <b className={styles.nome}>{chat.nome}</b>
+          <p className={styles.messagem}>{chat.mensagens.at(-1)}</p>
+        </div>
+        <div className={styles.detalhes_direita}>
+          <p className={styles.hora}>22:12</p>
+          <div className={styles.notificacoes}>
+            <span>{chat.mensagens.length}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatItem;
